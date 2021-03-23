4a0d9cfe5a4ab0374bf1b53cc60e7fb97b22cd91
julio <patatassaladaspepinillo@gmail.com>
2021-03-23 17:43:50 +0100

    p_h_p

--- public/basededades.php
+++ public/basededades.php
@@ -3,10 +3,10 @@ $servername="172.24.1.173";
 $database = "pokemons_daw";
 $username="root";
 $password="";
-// crear conexion
+//crear conexion
 $conn = mysqli_connect($servername, $username, $database);
-// Check connection
-if (!$conn) {
+//Check connection
+if(!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
