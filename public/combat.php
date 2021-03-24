1b7b9310b1fabbbc483e9213b7854fe98d4b9e5a
Carles <cquintanas8@boscdelacoma.cat>
2021-03-24 12:25:04 +0100

    Combat amb algo de BD

--- public/combat.php
+++ public/combat.php
@@ -8,8 +8,54 @@
     <!-- <script src="functions.js"></script> -->
 </head>
 
-<body style="background-image: url('índice.jpg'); background-size: cover">
-    <p> test  combat </p>
+<body style="background-image: url('Ă­ndice.jpg'); background-size: cover">
+    <?php
+		$servername="127.0.0.1";
+		$database = "pokemonapitest";
+		$username="root";
+		$password="";
+		// crear conexion
+		$conn = mysqli_connect($servername, $username, $password, $database);
+		// Check connection
+		if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());
+		}
+		//echo "Connected successfully";
+
+		//consulta
+		$consulta = "SELECT * FROM combat";
+		$resultado = mysqli_query( $conn, $consulta );
+		
+		$i = 1;
+		$arrayResultat = [];
+		while ($row = $resultado->fetch_array()){
+			if($i == $_GET["combat"]){
+				$consultaPokemonGuanyador = 'SELECT PokemonId FROM usuari WHERE Id = ' . $row['Guanyador'];
+				$resultat2 = mysqli_query( $conn, $consultaPokemonGuanyador);
+				$row2 = $resultat2->fetch_array();
+				$nomPokemon = 'SELECT Nom FROM pokemon WHERE id = ' . $row2[0];
+				$resultat3 = mysqli_query( $conn, $nomPokemon);
+				$row3 = $resultat3->fetch_array();
+				$j = 0;
+				$ArrayAccions = explode("#", $row['accions']);
+				$nom = [];
+				while ($j < count($ArrayAccions)){
+					$nom = explode("@", $ArrayAccions[$j]);
+					echo "En " . $nom[0] . " executa l'atac";
+					echo "<br />";
+					$j++;
+				}
+				echo "<h2> Guanyador ". $nom[0] ." amb el pokemon " . $row3[0] . "<h2>";
+			}
+			$i++;
+		}
+		
+
+
+
+		//cerrarconexion
+		mysqli_close( $conn );
+	?>
 
 </body>
 
